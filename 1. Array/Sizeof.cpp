97a466a53bef7e9c9ce430afc774446cdefe365e
tgarg5858 <tgarg5858@gmail.com>
2025-04-13 01:43:14 +0530

    Add files via upload

---
+++ 1. Array/Sizeof.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int array[] = {1,2,3,4,5};
+
+    cout<<sizeof(array)<<endl;
+    cout<<sizeof(array)/sizeof(array[0])<<endl;
+
+    // traversing through loop
+
+    for(int i=0; i<=4; i++){
+        cout<<array[i]<<endl;
+    }
+
+    return 0;
+}
