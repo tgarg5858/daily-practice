b483005c48b7e673ee89f040e36f7b4c12bfa991
tgarg5858 <tgarg5858@gmail.com>
2025-04-28 21:21:48 +0530

    ...

--- 1. Array/Sizeof.cpp
+++ 1. Array/Sizeof.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-    int array[] = {1,2,3,4,5};
+    int array[] = {1,2,3,4,5,6};
 
     cout<<sizeof(array)<<endl;
     cout<<sizeof(array)/sizeof(array[0])<<endl;
