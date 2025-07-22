31f31f52b7749e0c7156efdcd99e541980c19905
Tushar garg <tgarg5858@gmail.com>
2025-07-23 01:27:27 +0530

    Add files via upload

---
+++ file handling/fourth.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream file;
+    file.open("anil.txt");
+    if(!file.is_open()){
+        cout<<"error while opening the file";
+    }
+    else{
+        cout<<"reading from the file contents are"<<endl;
+        string line;
+        while(file.good()){
+            getline(file,line);
+            cout<<line<<endl;
+        }
+    }
+    return 0;
+}
